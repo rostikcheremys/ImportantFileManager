2add3f4b2d76f501b926a3beccad0c19647096ff
rostik <rostikcheremys@gmail.com>
2024-11-14 12:47:30 +0200

    delete ImportantFilesStorage

--- ImportantFileManager/Program.cs
+++ ImportantFileManager/Program.cs
@@ -2,8 +2,6 @@
 {
     static class ImportantFileManager
     {
-        private const string ImportantFilesStorage = "important_files.txt";
-
         static void Main(string?[] args)
         {
             string? command = args[0];
@@ -36,20 +34,19 @@
                 Console.WriteLine($"Error: File {filePath} does not exist.");
                 return;
             }
-
-            List<string?> importantFiles = GetImportantFiles();
             
-            if (importantFiles.Contains(filePath))
+            FileAttributes attributes = File.GetAttributes(filePath);
+            
+            if ((attributes & FileAttributes.Archive) != FileAttributes.Archive)
+            {
+                File.SetAttributes(filePath, attributes | FileAttributes.Archive);
+                Console.WriteLine($"File {filePath} marked as important.");
+            }
+            else
             {
                 Console.WriteLine($"File {filePath} is already marked as important.");
-                return;
             }
-
-            importantFiles.Add(filePath);
-            SaveImportantFiles(importantFiles);
-            Console.WriteLine($"File {filePath} marked as important.");
         }
-
 
         static void UnmarkFileAsImportant(string? filePath)
         {
@@ -58,49 +55,40 @@
                 Console.WriteLine($"Error: File {filePath} does not exist.");
                 return;
             }
-
-            List<string?> importantFiles = GetImportantFiles();
             
-            if (!importantFiles.Contains(filePath))
+            FileAttributes attributes = File.GetAttributes(filePath);
+            
+            if ((attributes & FileAttributes.Archive) == FileAttributes.Archive)
+            {
+                File.SetAttributes(filePath, attributes & ~FileAttributes.Archive);
+                Console.WriteLine($"File {filePath} unmarked as important.");
+            }
+            else
             {
                 Console.WriteLine($"File {filePath} is not marked as important.");
-                return;
             }
-
-            importantFiles.Remove(filePath);
-            SaveImportantFiles(importantFiles);
-            Console.WriteLine($"File {filePath} unmarked as important.");
         }
 
-        
         static void FindImportantFiles(string? directory, string? extension, string? nameContains)
         {
-            List<string?> importantFiles = GetImportantFiles();
-            List<string?> matchingFiles = importantFiles.Where(file => file!.StartsWith(directory!) && (extension == null || file.EndsWith($".{extension}")) && (nameContains == null || Path.GetFileName(file).Contains(nameContains))).ToList();
-
-            if (matchingFiles.Count == 0)
+            if (directory != null)
             {
-                Console.WriteLine("No important files found matching the criteria.");
-                return;
+                List<string> files = Directory.EnumerateFiles(directory, "*", SearchOption.AllDirectories).Where(file => 
+                        (File.GetAttributes(file) & FileAttributes.Archive) == FileAttributes.Archive && (extension == null || file.EndsWith($".{extension}")) && 
+                        (nameContains == null || Path.GetFileName(file).Contains(nameContains))).ToList();
+
+                if (files.Count == 0)
+                {
+                    Console.WriteLine("No important files found matching the criteria.");
+                }
+                else
+                {
+                    foreach (var file in files)
+                    {
+                        Console.WriteLine(Path.GetFullPath(file));
+                    }
+                }
             }
-
-            foreach (var file in matchingFiles)
-            {
-                Console.WriteLine(Path.GetFullPath(file!));
-            }
-        }
-
-        static List<string?> GetImportantFiles()
-        {
-            if (!File.Exists(ImportantFilesStorage)) return new List<string?>();
-
-            return File.ReadAllLines(ImportantFilesStorage).ToList()!;
-        }
-
-        static void SaveImportantFiles(List<string?> importantFiles)
-        {
-            File.WriteAllLines(ImportantFilesStorage, importantFiles!);
         }
     }
 }
-
